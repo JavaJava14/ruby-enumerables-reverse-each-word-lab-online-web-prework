b3a5bef4bc1d2d7825c075f4d4bd9d1a71d44f03
Jose Chavira <joeychavira2016@gmail.com>
2019-09-01 22:32:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(sentence)
-   sentence.split.each{|word| word.reverse}.join(" ") #=> "Hellothere,andhowareyou?"
+   sentence.each{|word| word.reverse}.join(" ") #=> "Hellothere,andhowareyou?"
   #sentence.split.collect{|word| word.reverse}.join(" ") #=> "olleH ,ereht dna woh era ?uoy"
 end
